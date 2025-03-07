2439dc608e4bd5ed7d32a64fb1b747b72deacb8e
Ian Buchanan <get.ian.buchanan@gmail.com>
2025-03-06 16:09:08 -0800

    feat: spec basic, tailwind journey begins

---
+++ config/tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js',
+    './app/components/**/*.{erb,rb}', // Add components directory
+    './config/initializers/simple_form.rb',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+};
